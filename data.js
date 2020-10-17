6a8f3b318c92ad1c9bf152dbcd854632727a6682
Mziyanda Joko <mziyandajoko@Mziyandas-MacBook-Pro.local>
2020-10-17 13:02:27 +0200

    Updates

---
+++ data.js
@@ -0,0 +1,20 @@
+export const data = [ 
+    {
+
+        Colors:[   "Red", "Blue", "Green", "Orange"   ],
+    },
+    {
+
+        Number:[ 13, 16, 18, 19],
+    },
+    {
+
+        Name :["Sarah", "Adam", "Ben", "John"],
+    },
+    {
+
+        Animal:[  "Dog", "Cat", "Mouse", "Tiger"],
+    }
+] 
+
+export const Colors = [ "Red", "Blue", "Green", "Orange"   ]
